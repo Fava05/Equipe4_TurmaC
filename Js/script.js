b0ef8c95abb6e1d1d3d4a07a6436644c88b64334
andrespleite <andrespl@al.insper.edu.br>
2024-11-21 18:25:57 -0300

    iterando a parte de dark mode do java

--- Js/script.js
+++ Js/script.js
@@ -1,12 +1,19 @@
 imgMode.addEventListener('click', () => {
-    const isDarkMode = imgMode.src.includes('DarkModeOFF.png');
+    // Verifica se está no modo escuro
+    const isDarkMode = imgMode.src.includes('DarkModeON.png');
+
+    // Altera a cor de fundo
     document.body.style.backgroundColor = isDarkMode ? '#5e5555' : '#FFFAFA';
+
+    // Altera a cor de todas as palavras em preto para branco
     document.body.style.color = isDarkMode ? '#ffffff' : '#000000';
 
+    // Seleciona todos os elementos com a classe 'resumo'
     const elementosResumo = document.querySelectorAll('.resumo');
+
+    // Muda a cor das palavras da classe 'resumo' para azul escuro ou azul claro
     elementosResumo.forEach(elemento => {
-        elemento.style.color = isDarkMode ? '#0845d4' : '#4dd2ff'; 
+        elemento.style.color = isDarkMode ? '#0845d4' : '#4dd2ff'; // Azul escuro no modo escuro, azul claro no modo claro
     });
 
 });
-
