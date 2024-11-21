cf8b081ef613d025bb2e2b4c78d85d0b2d684232
andrespleite <andrespl@al.insper.edu.br>
2024-11-21 18:03:52 -0300

    modifcando o java de darkmode

--- Js/script.js
+++ Js/script.js
@@ -2,22 +2,11 @@ imgMode.addEventListener('click', () => {
     const isDarkMode = imgMode.src.includes('DarkModeOFF.png');
     document.body.style.backgroundColor = isDarkMode ? '#5e5555' : '#FFFAFA';
     document.body.style.color = isDarkMode ? '#ffffff' : '#000000';
+
+    const elementosResumo = document.querySelectorAll('.resumo');
+    elementosResumo.forEach(elemento => {
+        elemento.style.color = isDarkMode ? '#0845d4' : '#4dd2ff'; 
+    });
+
 });
 
-//#0845d4 azul escuro
-
-imgMode.addEventListener('click', function () {
-    if (imgMode.src.includes('dark-mode-off.png')) {
-        document.body.style.backgroundColor = '#2B2B2B';
-        document.body.style.color = '#ffffff';
-        document.querySelectorAll('.card').forEach(card =>{
-            card.style.backgroundColor = '#334756';
-        });
-    } else {
-        document.body.style.backgroundColor = '#FFFAFA';
-        document.body.style.color = '#000000';
-        document.querySelectorAll('.card').forEach(card =>{
-            card.style.backgroundColor = '#FFFAFA';
-        });
-    }
-});
