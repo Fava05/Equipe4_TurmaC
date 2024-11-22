21caf1e32c695e451a5f7e056ecd974226c681cc
Guilherme <guilhermesl7@al.insper.edu.br>
2024-11-21 22:30:24 -0300

    COMMIT

--- Js/script.js
+++ Js/script.js
@@ -1,31 +1,11 @@
-const nomeSala = document.getElementById("nomeSala");
+document.addEventListener('DOMContentLoaded', function() {
+
+const nomeSala = document.querySelector(".main_nova_sala");
 const criarSala = document.getElementById("criarSala");
-const salaContainer = document.getElementById("salaContainer");
 
 criarSala.addEventListener("click", function () {
-    const nomeDigitado = nomeSala.textContent.trim();
-
-    if (nomeDigitado) {
-        // Cria um novo elemento
-        const novaSala = document.createElement("p");
-        const novoLink = document.createElement("a");
-
-        // Define os atributos e o conteúdo
-        novoLink.classList.add("baloes");
-        novoLink.href = `${nomeDigitado.toLowerCase().replace(/\s+/g, "_")}.html`;
-        novoLink.textContent = nomeDigitado;
-
-        // Adiciona o link ao parágrafo
-        novaSala.appendChild(novoLink);
-
-        // Adiciona o novo parágrafo ao container
-        salaContainer.appendChild(novaSala);
-
-        // Reseta o campo de entrada
-        nomeSala.textContent = "Nome da sala";
-    } else {
-        alert("Por favor, insira um nome para a sala.");
-    }
+    nomeSala.innerHTML = ''
 });
 
 
+})
